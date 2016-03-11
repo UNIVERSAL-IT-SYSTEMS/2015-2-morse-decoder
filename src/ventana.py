19d9d16c02cd82e82086728c11e0073296d2f43c
nhenriquezs <nmhenriquezs@gmail.com>
2016-03-10 22:40:57 -0300

    Nueva ventana y cambios en las gráficas

--- src/ventana.py
+++ src/ventana.py
@@ -1,9 +1,3 @@
-from graph import *
-from conv import *
-from filt import *
-from translator import *
-from decoder import *
-
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
@@ -15,8 +9,15 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 from matplotlib.figure import Figure
 from matplotlib import style
 
+from graph import *
+from conv import *
+from filt import *
+from translator import *
+from decoder import *
+
+
 LARGE_FONT= ("Verdana", 12)
-style.use("ggplot")
+#style.use("ggplot")
 forma = Figure(figsize=(2,2),dpi=100)
 forma2 = Figure(figsize=(2,2), dpi=100)
 
@@ -31,9 +32,9 @@ class ventanita(tk.Tk):
 		container.grid_columnconfigure(0, weight=1)
 		self.frames = {}
 
-		for F in (start_page, graphs_page):
-			frame = F(container, self)
-			self.frames[F] = frame
+		for pages in (start_page, graphs_page, graphs_page2):
+			frame = pages(container, self)
+			self.frames[pages] = frame
 			frame.grid(row=0, column=0, sticky="nsew")
 
 		self.show_frame(start_page)
@@ -41,6 +42,7 @@ class ventanita(tk.Tk):
 	def show_frame(self, cont):
 		frame = self.frames[cont]
 		frame.tkraise()
+
 
 def plotear_audio(forma=forma):
 	app.fileName = filedialog.askopenfilename( 
@@ -65,25 +67,11 @@ def plotear_audio(forma=forma):
 	plot_frecuencia(data,rate,forma)
 	spectrum(data,forma,rate)
 	plot_tiempo(data,rate,forma2,1)
-	print(decode_morse(".... --- .-.. .- / -- ..- -. -.. ---"))
-	print(encode_morse("hola mundo"))
+	#print(decode_morse(".... --- .-.. .- / -- ..- -. -.. ---"))
+	#print(encode_morse("hola mundo"))
 	#separar_audio(data,rate)
 
-	canvas = FigureCanvasTkAgg(forma2, app)
-	canvas.show()
-	canvas.get_tk_widget().pack(
-		side=tk.TOP, fill=tk.BOTH, expand = True
-	)
 
-	#barrita de funcionalidades de la gráfica
-	toolbar = NavigationToolbar2TkAgg(canvas, app)
-	toolbar.update()
-	canvas._tkcanvas.pack(
-		fill=tk.BOTH, expand = True
-	)
-
-	#label2 = tk.Label(app, text="Traducción... pronto", fg="blue", font=LARGE_FONT) #solo para dejar un espacio entre botón y gráfico
-	#label2.pack(pady=10) 
 
 class start_page(tk.Frame):
 
@@ -103,15 +91,37 @@ class start_page(tk.Frame):
 		boton_graph = ttk.Button(
 			self, text="Show more graphs", command=lambda: controller.show_frame(graphs_page) 
 		)
-		boton_graph.pack()
+		boton_graph.pack(padx=4, pady=10)
+
+		boton_graph2 = ttk.Button(
+			self, text="Show sound graph", command=lambda: controller.show_frame(graphs_page2) 
+		)
+		boton_graph2.pack()
+
+
+		self.canvas = Canvas(self)
+		self.canvas.pack(side = tk.TOP, fill = BOTH, expand = True)
+
+		canvas = FigureCanvasTkAgg(forma2, self)
+		canvas.show()
+		canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand = True)
+
+		#barrita de funcionalidades de la gráfica
+		toolbar = NavigationToolbar2TkAgg(canvas, self)
+		toolbar.update()
+		canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand = True)
+
+
+		
+
 
 class graphs_page(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self,parent)
 		label = tk.Label(self, text="Graph page", fg="red", font=LARGE_FONT)
 		label.pack(pady=20, padx=10)
-		boton2 = ttk.Button(self, text="Back to principal page", command=lambda: controller.show_frame(start_page))
-		boton2.pack()
+		boton_back = ttk.Button(self, text="Back to principal page", command=lambda: controller.show_frame(start_page))
+		boton_back.pack(padx=4, pady=10)
 
 		canvas = FigureCanvasTkAgg(forma, self)
 		canvas.show()
@@ -122,6 +132,25 @@ class graphs_page(tk.Frame):
 		toolbar.update()
 		canvas._tkcanvas.pack(fill=tk.BOTH, expand = True)
 
+
+class graphs_page2(tk.Frame):
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self,parent)
+		label = tk.Label(self, text="Graph page 2 - Sound graph", fg="black", font=LARGE_FONT)
+		label.pack(pady=20, padx=10)
+		boton_back = ttk.Button(self, text="Back to principal page", command=lambda: controller.show_frame(start_page))
+		boton_back.pack()
+
+		'''
+		canvas = FigureCanvasTkAgg(forma, self)
+		canvas.show()
+		canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand = True)
+
+		#barrita de funcionalidades de la gráfica
+		toolbar = NavigationToolbar2TkAgg(canvas, self)
+		toolbar.update()
+		canvas._tkcanvas.pack(fill=tk.BOTH, expand = True)
+		'''
 
 app = ventanita()
 app.minsize(width=1200, height=700)
